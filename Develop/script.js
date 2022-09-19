1033534663453c1beaa82f03b0c85c23cd1c547b
James <andritjm@gmail.com>
2022-09-19 17:28:45 -0500

    created variables and wrote function to determine length of password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
+var lowCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var upCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.>?/|";
+var numChar = "1234567890";
+var passLength
+var upCaseCheck
+var specialCharCheck
+var numCheck
+var character = lowCaseChar;
+console.log(character);
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function detLength() {
+  passLength = prompt("Choose a password length between 8 and 128 characters.");
+  if (isNaN(passLength)) {
+    alert("You must enter a value between 8 and 128!");
+    detLength();
+  } else if (passLength < 8 || passLength > 128) {
+    alert("Seriously. You need to enter a value between 8 and 128!");
+    detLength();
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
